7f01557fe6c253fed5fb88f8c66b40e45dad055c
Ivan Bachynin <bachynin.i@gmail.com>
2025-07-15 22:40:52 +0300

    user better naming

--- tf/main.tf
+++ tf/main.tf
@@ -18,20 +18,44 @@ data "ovh_cloud_project" "newsagg" {
   service_name = var.ovh_project_id
 }
 
-resource "ovh_cloud_project_user" "user" {
+# storage
+
+resource "ovh_cloud_project_user" "s3_user" {
   service_name = var.ovh_project_id
-  description  = "Object Storage User"
+  description  = "S3 Operator for newsagg objects storage"
   role_name    = "objectstore_operator"
 }
 
-resource "ovh_cloud_project_user_s3_credential" "admin" {
+resource "ovh_cloud_project_user_s3_credential" "s3_user_creds" {
   service_name = var.ovh_project_id
-  user_id      = ovh_cloud_project_user.user.id
+  user_id      = ovh_cloud_project_user.s3_user.id
 }
 
-resource "ovh_cloud_project_storage" "blob_storage" {
+resource "ovh_cloud_project_storage" "newsagg_bucket" {
   service_name = var.ovh_project_id
   region_name  = var.ovh_region
   name         = "newsagg"
-  owner_id     = ovh_cloud_project_user.user.id
+  owner_id     = ovh_cloud_project_user.s3_user.id
+  limit        = 0
+}
+
+resource "ovh_cloud_project_user_s3_policy" "s3_user_policy" {
+  service_name = var.ovh_project_id
+  user_id      = ovh_cloud_project_user.s3_user.id
+  policy = jsonencode({
+    "Statement" : [{
+      "Action" : ["s3:*"],
+      "Effect" : "Allow",
+      "Resource" : ["arn:aws:s3:::${ovh_cloud_project_storage.newsagg_bucket.name}", "arn:aws:s3:::${ovh_cloud_project_storage.newsagg_bucket.name}/*"],
+      "Sid" : "FullAccess"
+    }]
+  })
+}
+
+# VPS
+
+resource "ovh_cloud_project_ssh_key" "newsagg_admin" {
+  service_name = var.ovh_project_id
+  name         = "newsagg-admin-key"
+  public_key   = file("~/.ssh/id_rsa.pub")
 }
--- tf/output.tf
+++ tf/output.tf
@@ -2,17 +2,17 @@ output "_project_info" {
   value = data.ovh_cloud_project.newsagg
 }
 
-output "blob__storage_endpoint" {
-  value = ovh_cloud_project_storage.blob_storage.virtual_host
+output "blob_storage_endpoint" {
+  value = ovh_cloud_project_storage.newsagg_bucket.virtual_host
 }
 
 output "blob_access_key" {
   description = "the access key that have been created by the terraform script"
-  value       = ovh_cloud_project_user_s3_credential.admin.access_key_id
+  value       = ovh_cloud_project_user_s3_credential.s3_user_creds.access_key_id
 }
 
 output "blob_secret_key" {
   description = "the secret key that have been created by the terraform script"
-  value       = ovh_cloud_project_user_s3_credential.admin.secret_access_key
+  value       = ovh_cloud_project_user_s3_credential.s3_user_creds.secret_access_key
   sensitive   = true
 }
